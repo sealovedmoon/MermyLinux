71928acf62c69c1af38f9376112914b48f6c2297
sealovedmoon <thesealovedthemoon@gmail.com>
2025-02-01 20:32:44 +0200

    Update scripts

--- cleanbuild.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Run this script to clear WorkingDir then build the ISO in one go."
-
-sh ./clearworkdir.sh
-sh ./buildiso.sh
--- clearworkdir.sh
+++ clearworkdir.sh
@@ -2,7 +2,7 @@
 
 if [ ! -z "$( ls -A $HOME/MermyLinux-ISO/WorkingDir)" ]; then
 	echo "WorkingDir is being cleared..."
-	sudo rm -rf $HOME/MermyLinux-ISO/WorkingDir/*
+	sudo rm -rfv $HOME/MermyLinux-ISO/WorkingDir/*
 else
 	echo "WorkingDir is already empty!"
 fi
